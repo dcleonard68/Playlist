0a2ef9ba389e9ce7cb79d4de285c55283e360681
Chad Leonard <dchad.kari@gmail.com>
2016-07-07 13:53:53 -0600

    finished songController

---
+++ PlayList2/SongController.swift
@@ -0,0 +1,23 @@
+//
+//  SongController.swift
+//  PlayList2
+//
+//  Created by D Chad Leonard on 7/7/16.
+//  Copyright © 2016 D Chad Leonard. All rights reserved.
+//
+
+import Foundation
+
+class SongController {
+  
+  func createSong(name: String, artist: String, playlist: Playlist){
+    let song = Song(title: name, artist: artist)
+    playlistController.sharedController.addSongToPlaylist(song, playlist: playlist)
+    
+  }
+}
+
+
+
+
+
