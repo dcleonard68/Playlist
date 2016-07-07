08f3c8897752ecd106446b83d1886c6f32eb19c7
Chad Leonard <dchad.kari@gmail.com>
2016-07-07 12:12:48 -0600

    Added PlaylistController

---
+++ PlayList2/PlaylistController.swift
@@ -0,0 +1,37 @@
+//
+//  PlaylistController.swift
+//  PlayList2
+//
+//  Created by D Chad Leonard on 7/7/16.
+//  Copyright © 2016 D Chad Leonard. All rights reserved.
+//
+
+import Foundation
+
+class playlistController {
+  
+  var playlists: [Playlist] = []
+  
+  // CRUD (Create, Read, Update, Delete)
+  
+  func addPlaylist(name: String) {
+    let playlist = Playlist(title: name)
+    playlists.append(playlist)
+  }
+  
+  func deletePlaylist(playlist: Playlist) {
+    if let index = playlists.indexOf(playlist){
+      playlists.removeAtIndex(index)
+      
+    }
+  }
+  
+}
+
+
+
+
+
+
+
+
